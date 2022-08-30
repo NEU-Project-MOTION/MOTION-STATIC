b7603407e009d2598083af0c74322bc4d2a47df8
michaeltshen <shen.mich@northeastern.edu>
2022-08-30 18:43:46 -0400

    Cleanup

--- scripts/get_sample_rosbag.sh
+++ scripts/get_sample_rosbag.sh
@@ -5,3 +5,5 @@ wget https://autoware-ai.s3.us-east-2.amazonaws.com/sample_moriyama_data.tar.gz
 wget https://autoware-ai.s3.us-east-2.amazonaws.com/sample_moriyama_150324.tar.gz
 tar zxfv sample_moriyama_150324.tar.gz
 tar zxfv sample_moriyama_data.tar.gz 
+rm sample_moriyama_150324.tar.gz
+rm sample_moriyama_data.tar.gz
