5efad32cb2664c9a5959afe7035500ea13001a7e
michaeltshen <shen.mich@northeastern.edu>
2022-09-20 19:32:43 +0000

    Update script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,9 @@ pip install virtualenv
 virtualenv motion-venv
 source motion-venv/bin/activate
 pip install -r requirements.txt
+pip install -r examples/dash-image-processing/requirements.txt
+pip install -r examples/dash-manufacture-spc-dashboard/requirements.txt
+pip install -r examples/dash-object-detection/requirements.txt
 venv_activate=$(pwd)
 echo "alias capstone_venv='source $venv_activate/motion-venv/bin/activate'" >> ~/.bashrc 
 source ~/.bashrc
